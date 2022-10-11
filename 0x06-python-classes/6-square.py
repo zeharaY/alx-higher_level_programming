6f8a86ee6ac3259e3fe728ab51c870f18a400bc7
zeharaY <yassinzehara@gmail.com>
2022-10-11 08:13:27 -0700

    com

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,7 +2,7 @@
 """Class"""
 
 class Square:
-
+    """Class Aquare"""
     def __init__(self, size=0, position=(0, 0)):
 
         self.size = size
@@ -10,10 +10,12 @@ class Square:
 
     @property
     def size(self):
+        """size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """size with value"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -22,10 +24,12 @@ class Square:
 
     @property
     def position(self):
+        """position"""
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """position self,valus"""
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
@@ -37,6 +41,7 @@ class Square:
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Area"""
         if self.__size == 0:
             print("")
             return
