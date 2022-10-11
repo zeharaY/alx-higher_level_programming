f37e2d2ef843e956e7dd02c2318fc333e619dcff
zeharaY <yassinzehara@gmail.com>
2022-10-11 07:59:41 -0700

    com

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Class"""
 
 class Square:
-
+    """Class Square"""
     def __init__(self, size=0):
 
         if not isinstance(size, int):
@@ -11,4 +12,5 @@ class Square:
         self.__size = size
 
     def area(self):
+        """Area of Square"""
         return (self.__size * self.__size)
