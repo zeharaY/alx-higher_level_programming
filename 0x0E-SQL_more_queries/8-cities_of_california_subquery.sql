7d73215e7ee8c424d9d3a24690fdfaa1b00baaf6
ZeharaY <106754679+zeharaY@users.noreply.github.com>
2022-11-30 15:38:59 +0300

    com
    com

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Lists ascendingly all available cities of California from database hbtn_0d_usa.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,2 @@
+#alx-higher_level_programming
+0x0E-SQL_more_queries
